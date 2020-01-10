0bd51a34987310467c70c97dc0e9b1aa73f52554
zavr-1 <zavr-1@users.noreply.github.com>
2020-01-10 05:57:35 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('splendid').Page}
+ */
+export const index = {
+  title: 'MyNewPackage',
+  seo: 'The 150-160 characters search engine meta:description',
+  og: {
+    image: '/img/logo.jpg',
+  },
+  links: {
+    gutenberg: 'https://www.gutenberg.org/files/84/84-h/84-h.htm',
+  },
+}
+
+/**
+ * A subdirectory with more pages should be exported as a
+ * string constant.
+ */
+export const help = '~/help'
